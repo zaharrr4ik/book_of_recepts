079cf605d6a689386832c696903a63a74910a595
zaharrr4ik <ymnuichelovek12@gmail.com>
2025-05-12 00:47:01 +0300

    Delete lms directory

--- lms/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-SECRET_KEY = os.getenv('SECRET_KEY', 'secret123')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp3', 'wav'}
--- lms/server.py
+++
@@ -1,6 +0,0 @@
-# Просто импортируем app из основного файла
-from app import app
-
-# Для совместимости с Glitch
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
